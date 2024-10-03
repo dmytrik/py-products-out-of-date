8af3ab934e3d967e8e967a93b389e0450b13ae99
dmytro <dmysamo@gmail.com>
2024-10-03 16:10:33 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,68 +2,72 @@ from unittest import mock
 from app.main import outdated_products
 from datetime import date
 
+
 @mock.patch("app.main.datetime")
-def test_outdated_products_on_all_correct(m_date):
+def test_outdated_products_on_all_correct(m_date: object) -> None:
     m_date.date.today.return_value = date(2022, 2, 2)
-    products = [
-                    {
-                        "name": "salmon",
-                        "expiration_date": date(2022, 2, 10),
-                        "price": 600
-                    },
-                    {
-                        "name": "chicken",
-                        "expiration_date": date(2022, 2, 5),
-                        "price": 120
-                    }
-                ]
+    products =\
+        [
+            {
+                "name": "salmon",
+                "expiration_date": date(2022, 2, 10),
+                "price": 600
+            },
+            {
+                "name": "chicken",
+                "expiration_date": date(2022, 2, 5),
+                "price": 120
+            }
+        ]
     result = outdated_products(products)
     assert result == []
 
 
 @mock.patch("app.main.datetime")
-def test_outdated_products_on_one_mistake(m_date):
+def test_outdated_products_on_one_mistake(m_date: object) -> None:
     m_date.date.today.return_value = date(2022, 2, 2)
-    products = [
-                    {
-                        "name": "salmon",
-                        "expiration_date": date(2022, 2, 10),
-                        "price": 600
-                    },
-                    {
-                        "name": "chicken",
-                        "expiration_date": date(2022, 2, 5),
-                        "price": 120
-                    },
-                    {
-                        "name": "duck",
-                        "expiration_date": date(2022, 2, 1),
-                        "price": 160
-                    }
-                ]
+    products =\
+        [
+            {
+                "name": "salmon",
+                "expiration_date": date(2022, 2, 10),
+                "price": 600
+            },
+            {
+                "name": "chicken",
+                "expiration_date": date(2022, 2, 5),
+                "price": 120
+            },
+            {
+                "name": "duck",
+                "expiration_date": date(2022, 2, 1),
+                "price": 160
+            }
+        ]
     result = outdated_products(products)
     assert result == ["duck"]
 
 
 @mock.patch("app.main.datetime")
-def test_outdated_products_on_all_mistake(m_date):
+def test_outdated_products_on_all_mistake(m_date: object) -> None:
     m_date.date.today.return_value = date(2022, 2, 2)
-    products = [
-                    {
-                        "name": "salmon",
-                        "expiration_date": date(2022, 1, 10),
-                        "price": 600
-                    },
-                    {
-                        "name": "chicken",
-                        "expiration_date": date(2022, 1, 5),
-                        "price": 120
-                    },
-                    {
-                        "name": "duck",
-                        "expiration_date": date(2022, 1, 1),
-                        "price": 160
-                    }
-                ]
+    products =\
+        [
+            {
+                "name": "salmon",
+                "expiration_date": date(2022, 1, 10),
+                "price": 600
+            },
+            {
+                "name": "chicken",
+                "expiration_date": date(2022, 1, 5),
+                "price": 120
+            },
+            {
+                "name": "duck",
+                "expiration_date": date(2022, 1, 1),
+                "price": 160
+            }
+        ]
     result = outdated_products(products)
-    assert result == ['salmon', 'chicken', 'duck']
+    assert result == ["salmon", "chicken", "duck"]
